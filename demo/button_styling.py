e6eb15bed75423a7bafe66e97369812e941b77b7
timeyyy <timeyyy_da_man@hotmail.com>
2017-04-27 16:34:10 +0200

    Cleanup button_styling a bit

--- demo/button_styling.py
+++ demo/button_styling.py
@@ -1,28 +1,3 @@
-#~ BSD 3-Clause License (Revised)
-#~ 
-#~ Copyright 2015 Timothy C Eichler (c) , All rights reserved.
-#~ 
-#~ Redistribution and use in source and binary forms, with or without modification, are 
-#~ permitted provided that the following conditions are met:
-#~ 
-#~ 1. Redistributions of source code must retain the above copyright notice, this list of 
-#~ conditions and the following disclaimer.
-#~ 2. Redistributions in binary form must reproduce the above copyright notice, this list
-#~ of conditions and the following disclaimer in the documentation and/or other materials 
-#~ provided with the distribution. 
-#~ 3. Neither the name of the nor the names of its contributors may be used to endorse or 
-#~ promote products derived from this software without specific prior written permission. 
-#~ 
-#~ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS 
-#~ OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
-#~ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
-#~ COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FORANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-#~ EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
-#~ GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
-#~ AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILIT, OR TORT (INCLUDING 
-#~ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
-#~ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import PhotoImage
@@ -30,15 +5,11 @@ import os
 import glob
 
 colors = {
-	#~ "frame": "#9ee874",	# a green color for testing etc 
-	#~ "disabledfg": "#9ee874",
-	#~ "selectbg": "#9ee874",
-	#~ "selectfg": "#9ee874"
-	"frame": "#efefef",
-	"disabledfg": "#aaaaaa",
-	"selectbg": "#657a9e",
-	"selectfg": "#ffffff"
-	}
+    "frame": "#efefef",
+    "disabledfg": "#aaaaaa",
+    "selectbg": "#657a9e",
+    "selectfg": "#ffffff"
+}
 
 imgs = {}
 def _load_imgs(imgdir):			# Images without any references are garbage collected by python
@@ -53,21 +24,7 @@ def _load_imgs(imgdir):			# Images without any references are garbage collected 
 def install(root, imgdir):
 	_load_imgs(imgdir)
 	style=ttk.Style(root)
-	
 	ttk.Style().theme_create("toggle", "default", settings={
-		#~ ".":		
-		#~ {
-			#~ "configure":
-				#~ {"background": colors['frame'],
-				 #~ "troughcolor": colors['frame'],
-				 #~ "selectbackground": colors['selectbg'],
-				 #~ "selectforeground": colors['selectfg'],
-				 #~ "fieldbackground": colors['frame'],
-				 #~ "font": "TkDefaultFont",
-				 #~ "borderwidth": 1},
-			#~ "map": {"foreground": [("disabled", colors['disabledfg'])]}
-		#~ }, 
-		
 		"ToggleButton": {
 			"configure": {"width": 10, "anchor": "center"},
 			"layout": [
@@ -80,7 +37,6 @@ def install(root, imgdir):
 				})
 			]
 		},
-		
 		"ToggleButton.button": {"element create":
 			("image", 'button-n',
 		("pressed", 'button-p'), ("active","!alternate", 'button-h'),
@@ -91,4 +47,3 @@ def install(root, imgdir):
 	}
 	)
 	style.theme_use('toggle')
-	
